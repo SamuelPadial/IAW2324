6f3867bfa241356a8d319309ff8e5b66b528a1ad
SamuelPadial <114154158+SamuelPadial@users.noreply.github.com>
2023-12-12 00:38:26 +0100

    Tipos de datos añadido

---
+++ tiposdedatos.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos de datos</title>
+</head>
+<body>
+    <?php
+    $m1 = 3;
+    $m2 = 1.53;
+    $m3 = "Esto es una cadena";
+    $m4 = True;
+    $m5 = ["a1","a2","a3"];
+
+    var_dump($m1);
+    var_dump($m2);
+    var_dump($m3);
+    var_dump($m4);
+    var_dump($m5);
+    ?>
+</body>
+</html>
